938ac29d9f87189d66fabc056a669f84e8d066ac
Nagaraj <141642898+nagaraju2508@users.noreply.github.com>
2026-07-19 23:25:13 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() !=t.length())
+            return false;
+        HashMap<Character,Integer> hmap1=new HashMap<>();
+        HashMap<Character,Integer> hmap2=new HashMap<>();
+        for(char c:s.toCharArray())
+            hmap1.put(c,hmap1.getOrDefault(c,0)+1);
+        for(char c:t.toCharArray())
+            hmap2.put(c,hmap2.getOrDefault(c,0)+1);
+        return hmap1.equals(hmap2);
+        
+    }
+}
